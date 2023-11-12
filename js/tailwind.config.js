ad3c0c53ace3e502c2bc19252353d46bedab978a
alaminusa <aamin1103@gmail.com>
2023-11-12 20:33:56 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+
+tailwind.config =  {
+    theme: {
+      extend: {
+        
+        fontFamily: {
+          'laila': ['Laila', 'sans-serif'],
+        },
+        // Font Family end
+
+        // Font Family start
+        backgroundImage: {
+          'cover-bg': "url('./image/coverbg.png')",
+        },
+      },
+    },
+  };
